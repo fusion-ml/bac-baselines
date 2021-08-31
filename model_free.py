6c85bdc315bc97353306db187f73972047029d5a
Viraj Mehta <virajm@cs.cmu.edu>
2021-08-31 10:20:17 -0400

    got all implementations working

--- model_free.py
+++ model_free.py
@@ -1,4 +1,5 @@
 import gym
+from gym.wrappers import TimeLimit
 import bax.envs
 from bax.util.misc_util import Dumper
 from stable_baselines3 import PPO, SAC, TD3
@@ -11,10 +12,12 @@ import hydra
 def main(config):
     env = gym.make(config.env.name)
     eval_env = gym.make(config.env.name)
+    env = TimeLimit(env, env.horizon)
+    eval_env = TimeLimit(eval_env, eval_env.horizon)
     check_env(env)
     model_classes = {'PPO': PPO, 'SAC': SAC, 'TD3': TD3}
     model_class = model_classes[config.alg.name]
-    model = model_class('MlpPolicy', env)
+    model = model_class('MlpPolicy', env, verbose=1)
     eval_callback = EvalCallback(eval_env, best_model_save_path='.',
                                  log_path='.', eval_freq=config.eval_frequency,
                                  deterministic=True, render=False, n_eval_episodes=config.num_eval_trials)
